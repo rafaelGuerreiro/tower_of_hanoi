f091dcc4f07f30eaa818a652791d835ffe594bfb
Rafael Rubem Rossi Souza Guerreiro <guerreiro.dev@gmail.com>
2016-09-27 09:37:09 -0700

    Add query selector

--- js/game_framework.js
+++ js/game_framework.js
@@ -2,26 +2,86 @@
 ;(function(document, window, undefined) {
   'use strict';
 
-  window.$ = querySelectorAll;
+  var EMPTY_NODES = toNodesList();
 
-  // main functions
+  Array.prototype.each = each;
+  Array.prototype.map = map;
 
+  var $ = querySelectorAll;
+
+  $.isEmpty = isEmpty;
+  $.isPresent = isPresent;
+  $.isStringPresent = isStringPresent;
+  $.isFunction = isFunction;
+
+  window.$ = $;
+
+  // Main functions
   function querySelectorAll(selector, parent) {
-    if ()
+    if (isEmpty(selector))
+      return EMPTY_NODES;
 
-    if (typeof parent === 'string')
-      parent = querySelectorAll(parent);
+    if (isStringPresent(parent))
+      return toNodesList(querySelectorAll(parent).map(function() {
+        return querySelectorAll(this, selector);
+      }));
 
-    return document.querySelectorAll(selector);
+    return toNodesList(document.querySelectorAll(selector));
   }
 
-  // helper functions
+  function toNodesList(nodes) {
+    if (!nodes)
+      nodes = [];
 
+    return Array.isArray(nodes) ? nodes : [nodes];
+  }
+
+  // Helper functions
   function isEmpty(value) {
     if (!value)
       return true;
 
-    return value;
+    if (typeof value === 'string')
+      value = value.trim();
+
+    return !value;
   }
 
+  function isPresent(value) {
+    return !isEmpty(value);
+  }
+
+  function isStringPresent(value) {
+    return typeof value === 'string' && isPresent(value);
+  }
+
+  function isFunction(fn) {
+    return isPresent(fn) && typeof fn === 'function';
+  }
+
+  // Objects improvement
+  function each(callback) {
+    if (!isFunction(callback))
+      return this;
+
+    for (var index = 0; index < this.length; index++) {
+      var response = callback.call(this[index], index, this);
+
+      if (response === false)
+        break;
+    }
+
+    return this;
+  }
+
+  function map(callback) {
+    if (!isFunction(callback))
+      return [];
+
+    var map = [];
+    for (var index = 0; index < this.length; index++)
+      map.push(callback.call(this[index], index, this));
+
+    return map;
+  }
 })(document, window);
